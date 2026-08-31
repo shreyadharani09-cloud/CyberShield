9cad96a236e07b58799e8fcde51fa0e3bca9632a
Shreya Dharani <shreya.dharani09@gmail.com>
2026-08-31 10:59:10 +0530

    Fix register import case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import NewsDetails from "./pages/newsDetails";
 import Login from "./pages/login";
-import Register from "./pages/Register";
+import Register from "./pages/register";
 
 function App() {
   return (
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://192.168.1.8:5000/api",
-});
+axios.create({
+  baseURL: "https://cybershield-sax5.onrender.com/api"
+})
 
 export default api;
