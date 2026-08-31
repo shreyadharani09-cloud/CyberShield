3f2a3a5d2bc766bf0ac27da625f11d6f092977f7
Shreya Dharani <shreya.dharani09@gmail.com>
2026-08-31 11:19:12 +0530

    Fix component filename case

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 import Navbar from "../components/navbar";
-import NewsCard from "../components/newsCard";
+import NewsCard from "../components/newscard";
 import Stats from "../components/stats";
 
 function Home() {
