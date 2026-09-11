ca2533d0d199439e7eb16ed5ee541deb01d8b854
Shreya Dharani <shreya.dharani09@gmail.com>
2026-09-11 20:41:13 +0530

    Update backend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cybershield-sax5.onrender.com/api"
+  baseURL: "https://cybershieldserver-sax5.onrender.com/api"
 });
 
 export default api;
