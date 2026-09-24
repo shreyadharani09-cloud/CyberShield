93b2d6598bda66214146f203e67730fee5691f3e
Shreya Dharani <shreya.dharani09@gmail.com>
2026-09-24 20:42:42 +0530

    Use Gmail SMTP port 587

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -2,9 +2,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    family: 4,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
